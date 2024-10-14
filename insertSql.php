01e0187dda151764b89900f8eada3520cb584678
Akshay-C-A <akshayanil3103@gmail.com>
2024-10-14 09:42:36 +0530

    edited insertion

--- insertSql.php
+++ insertSql.php
@@ -5,6 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
+<body>
+    <form action="insertSql.php" method="post">
+        <label for="testfield">Text to Add: </label>
+        <input type="text" name="testfield" size="30">
+        <input type="submit" value="Insert Record" name="submit">
+    </form>
+</body>
+</html>
+
 <?php
     $conn = mysqli_connect("localhost","root","","test1");
     if($conn)
@@ -27,11 +37,3 @@
     }
     mysqli_close($conn);
 ?>
-<body>
-    <form action="insertSql.php" method="post">
-        <label for="testfield">Text to Add: </label>
-        <input type="text" name="testfield" size="30">
-        <input type="submit" value="Insert Record" name="submit">
-    </form>
-</body>
-</html>
